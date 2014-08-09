3ec798c85b9f9dbadd8dabe5f64654144ac2a6df
Tony Walker <tony.walker@local>
2014-08-08 21:56:13 -0700

    Set security related headers by default

--- app/src/handlers.py
+++ app/src/handlers.py
@@ -7,6 +7,15 @@ import tornado.web
 class BaseHandler(tornado.web.RequestHandler):
     """ To be inherited by other handlers """
 
+    def set_custom_headers(self):
+        """ Set some custom and security related headers """
+        self.set_header('x-frame-options', 'DENY')
+        self.set_header('x-xss-protection', '1; mode=block')
+
+    def prepare(self):
+        super(BaseHandler, self).prepare()
+        self.set_custom_headers()
+
     def write_json(self, data, status_code=200):
         """ Write json data to the client """
         self.set_header('Content-Type', 'application/json; charset=UTF-8')
